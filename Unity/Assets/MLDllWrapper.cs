0dafac846e6f88a0a068558855d29ef31b867ad2
ArthurJacquin <artuj38@free.fr>
2020-10-26 13:06:29 +0100

    Unity project

---
+++ Unity/Assets/MLDllWrapper.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Runtime.InteropServices;
+
+public static class MlDllWrapper
+{
+    [DllImport("MlDll", EntryPoint = "my_add")]
+    public static extern double MyAdd(double a, double b);
+    
+    [DllImport("MlDll", EntryPoint = "create_linear_model")]
+    public static extern System.IntPtr CreateLinearModel(int inputSize);
+    
+    [DllImport("MlDll", EntryPoint = "predict_linear_model_classification")]
+    public static extern double PredictLinearModelClassification(System.IntPtr model,
+        double[] inputs, int inputSize);
+    
+    [DllImport("MlDll", EntryPoint = "predict_linear_model_multiclass_classification")]
+    public static extern System.IntPtr PredictLinearModelMultiClassClassification(System.IntPtr model,
+        double[] inputs, int inputSize, int classCount);
+    
+    [DllImport("MlDll", EntryPoint = "delete_linear_model")]
+    public static extern void DeleteLinearModel(System.IntPtr model);
+}
---
+++ Unity/Assets/TestMlDll.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class TestMlDll : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log(MlDllWrapper.MyAdd(42.0, 51.0));
+        
+    }
+
+    private void Update()
+    {
+        var model = MlDllWrapper.CreateLinearModel(4096);
+        var inputs = new double[] {1.0, 2.0, 3.0};
+        var result = MlDllWrapper.PredictLinearModelMultiClassClassification(model,
+            inputs, 3, 3);
+        
+        var result_managed = new double[3];
+        
+        Marshal.Copy(result, result_managed, 0, 3);
+
+        foreach (var rslt in result_managed)
+        {
+            Debug.Log(rslt);
+        }
+        
+        MlDllWrapper.DeleteLinearModel(result);
+        
+        MlDllWrapper.DeleteLinearModel(model);
+    }
+}

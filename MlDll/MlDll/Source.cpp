4d8f9ddf53c69baf991ce663bdd7760a5fee700e
ArthurJacquin <artuj38@free.fr>
2020-10-27 20:14:30 +0100

    dataset real crop

--- MlDll/MlDll/Source.cpp
+++ MlDll/MlDll/Source.cpp
@@ -1,9 +1,21 @@
 #include <cstdlib>
+#include <iostream>
+#include <time.h>
 
+double Sign(double x)
+{
+	return 1 / (1 + exp(-x));
+}
 
 extern "C" {
+
+	/// <summary>
+	/// init des poids random
+	/// </summary>
 	__declspec(dllexport) double* create_linear_model(int inputs_count) {
 		auto weights = new double[inputs_count + 1];
+		srand(time(NULL));
+
 		for (auto i = 0; i < inputs_count + 1; i++) {
 			weights[i] = rand() / (double)RAND_MAX * 2.0 - 1.0;
 		}
@@ -11,28 +23,62 @@ extern "C" {
 		return weights;
 	}
 
-	__declspec(dllexport) double predict_linear_model_classification(double* model, double inputs[], int inputs_count) {
-		// TODO
-		return -1;
+	__declspec(dllexport) double predict_linear_model_classification(double* model, double samples[], int input_count) {
+		
+		double sum = model[0]; //poids du biais
+
+		//somme de tout les samples * poids
+		for (size_t i = 0; i < input_count; i++)
+		{
+			sum += samples[i] * model[i + 1];
+		}
+
+		//Sigmoid
+		if(Sign(sum) < 0.5)
+			return -1.0;
+		
+		return 1.0;
 	}
 
-	__declspec(dllexport) double* predict_linear_model_multiclass_classification(double* model, double inputs[], int inputs_count,
+	__declspec(dllexport) double* predict_linear_model_multiclass_classification(double* model, double samples[], int sample_count,
 		int class_count) {
 		// TODO
 		return new double[3] {1.0, -1.0, 1.0};
 	}
 
-	__declspec(dllexport) void train_linear_model_rosenblatt(double* model, double all_inputs[], int inputs_count, int sample_count,
-		double all_expected_outputs[], int expected_output_size, int epochs, double learning_rate) {
-		// TODO
+	__declspec(dllexport) void train_linear_model_Rosenblatt(double* model, double all_samples[], int sample_count, int input_count,
+		double all_expected_outputs[], int epochs, double learning_rate) {
+		
+		srand(time(NULL));
+
+		//Repeter N fois
+		for (size_t it = 0; it < epochs; it++)
+		{
+			int k = rand() % (sample_count); //Choix d'un indice random
+			
+			//Recup des inputs (X, Y)
+			double* X = new double[input_count];
+			for (size_t i = 0; i < input_count; i++)
+				X[i] = all_samples[k * input_count + i];
+
+			double Y = all_expected_outputs[k]; //Recup du resultat souhaité
+			
+			double p = predict_linear_model_classification(model, X, input_count); //Prediction du résultat
+
+			//Mise à jour des poids
+			for (size_t i = 0; i < input_count; i++)
+			{
+				if(it > 9990)
+					std::cout << "k : " << k << " | Y : " << Y << " | p : " << p << " | X[i] : " << X[i] << " = " << learning_rate * (Y - p) * X[i] << std::endl;
+
+				model[i + 1] += learning_rate * (Y - p) * X[i];
+			}
+		}
+
 		return;
 	}
 
 	__declspec(dllexport) void delete_linear_model(double* model) {
 		delete[] model;
-	}
-
-	__declspec(dllexport) double my_add(double a, double b) {
-		return a + b + 2;
 	}
 }
--- MlDll/TestMLDLL/TestMLDLL.cpp
+++ MlDll/TestMLDLL/TestMLDLL.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
+#include "Test.h"
 
 extern "C"
 {
-    __declspec(dllimport) double my_add(double a, double b);
+    __declspec(dllimport) double* create_linear_model(int inputs_count);
+    __declspec(dllimport) double predict_linear_model_classification(double* model, double inputs[], int sample_count);
+    __declspec(dllimport) void train_linear_model_Rosenblatt(double* model, double all_samples[], int sample_count, int input_count,
+        double all_expected_outputs[], int epochs, double learning_rate);
+    __declspec(dllimport) void delete_linear_model(double* model);
 }
 
 int main()
 {
-    std::cout << "Hello World!\n" << my_add(42, 51) << std::endl;
+    std::cout.precision(5);
+    getchar();
+
+    CasTest test(TestType::LinearSimple);
+    test.DisplayInfos();
+
+    double* model = create_linear_model(2);
+    std::cout << "Weights : " << model[0] << ", " << model[1] << ", " << model[2] << std::endl;
+    
+
+    std::cout << "BEFORE !" << std::endl;
+    for (size_t i = 0; i < test.sample_count; i ++)
+    {
+        std::cout << predict_linear_model_classification(model, new double[2]{ test.samples[i], test.samples[i + 1] }, 2) << std::endl;
+    }
+
+    train_linear_model_Rosenblatt(model, test.samples, test.sample_count, 2, test.outputs, 1000, 0.1);
+    std::cout << "Weights : " << model[0] << ", " << model[1] << ", " << model[2] << std::endl;
+
+    std::cout << "AFTER !" << std::endl;
+    for (size_t i = 0; i < test.sample_count; i++)
+    {
+        std::cout << predict_linear_model_classification(model, new double[2]{ test.samples[i], test.samples[i + 1] }, 2) << std::endl;
+    }
+    
+    delete_linear_model(model);
 }

2b96e900a6a9ca94433ad1985e76b483ee2c27ba
ArthurJacquin <artuj38@free.fr>
2020-10-27 20:18:12 +0100

    Linear perceptron implemented but not working

---
+++ MlDll/TestMLDLL/Test.cpp
@@ -0,0 +1,86 @@
+#include "Test.h"
+#include <iostream>
+#include <time.h>
+
+CasTest::CasTest(TestType t)
+{
+	srand(time(NULL));
+	switch (t)
+	{
+	case LinearSimple:
+		sample_count = 3;
+		samples = new double[sample_count * 2]{ 1.0, 1.0, 2.0, 3.0, 3.0, 3.0 };
+		outputs = new double[sample_count] { 1, -1, -1 };
+
+		break;
+
+	case LinearMultiple:
+		sample_count = 100;
+		samples = new double[sample_count * 2];
+		for (size_t i = 0; i < 200; i++)
+		{
+			if (i < 100)
+				samples[i] = rand() / (double)RAND_MAX + 1.0;
+			else
+				samples[i] = rand() / (double)RAND_MAX + 2.0;
+		}
+
+		outputs = new double[sample_count];
+		for (size_t i = 0; i < 100; i++)
+		{
+			if (i < 50)
+				outputs[i] = 1.0;
+			else
+				outputs[i] = -1.0;
+		}
+		break;
+
+	case XOR:
+		sample_count = 4;
+		samples = new double[sample_count * 2]{ 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0 };
+		outputs = new double[sample_count] { 1, 1, -1, -1 };
+		break;
+
+	case Cross:
+
+		break;
+	case MultiLinear:
+
+		break;
+	case MultiCross:
+
+		break;
+	case LinearSimple2D:
+
+		break;
+	case NonLinearSimple2D:
+
+		break;
+	case LinearSimple3D:
+
+		break;
+	case LinearTricky3D:
+
+		break;
+	case NonLinearSimple3D:
+
+		break;
+
+	default:
+		samples = new double[0];
+		outputs = new double[0];
+		break;
+	}
+}
+
+void CasTest::DisplayInfos()
+{
+	std::cout.precision(5);
+
+	std::cout << "Samples count : " << sample_count <<std::endl;
+	std::cout << "   Samples     |      Output " << std::endl;
+	for (size_t i = 0; i < sample_count; i++)
+	{
+		std::cout << samples[i * 2] << ", " << samples[i * 2 + 1] << " | " << outputs[i] << std::endl;
+	}
+}

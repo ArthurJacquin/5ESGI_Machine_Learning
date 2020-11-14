4bc83ed35cb1d142358f3d27b51c0765ccbb3ecc
Attika <attika.mayou@gmail.com>
2020-11-14 13:56:37 +0100

    Test class moved on unity and not on dll

---
+++ MlDll/TestMLDLL/Test.cpp
@@ -0,0 +1,114 @@
+#include "Test.h"
+#include <iostream>
+#include <time.h>
+
+CasTest::CasTest(TestType t)
+{
+	srand(time(NULL));
+	switch (t)
+	{
+	case LinearSimple:
+		sample_count = 3;
+		samples = new double[sample_count * 2]{ 1.0, 1.0, 2.0, 3.0, 3.0, 3.0 };
+		outputs = new double[sample_count] { 1, -1, -1 };
+		break;
+
+	case LinearMultiple:
+		sample_count = 100;
+		samples = new double[sample_count * 2];
+		for (size_t i = 0; i < 200; i++)
+		{
+			if (i < 100)
+				samples[i] = rand() / (double)RAND_MAX + 1.0;
+			else
+				samples[i] = rand() / (double)RAND_MAX + 2.0;
+		}
+
+		outputs = new double[sample_count];
+		for (size_t i = 0; i < 100; i++)
+		{
+			if (i < 50)
+				outputs[i] = 1.0;
+			else
+				outputs[i] = -1.0;
+		}
+		break;
+
+	case XOR:
+		sample_count = 4;
+		samples = new double[sample_count * 2]{ 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0 };
+		outputs = new double[sample_count] { 1, 1, -1, -1 };
+		break;
+
+	case Cross:
+		sample_count = 500;
+		samples = new double[sample_count * 2];
+		for (size_t i = 0; i < (sample_count * 2); i++)
+		{
+			samples[i] = rand() / (double)RAND_MAX * 2.0 - 1.0;
+		}
+
+		outputs = new double[sample_count];
+		for (size_t i = 0; i < sample_count; i++)
+		{
+			if (abs(samples[i * 2]) <= 0.3 || abs(samples[i * 2 + 1]) <= 0.3)
+				outputs[i] = 1.0;
+			else
+				outputs[i] = -1.0;
+		}
+		break;
+
+	case MultiLinear:
+		break;
+
+	case MultiCross:
+		break;
+
+	case LinearSimple2D:
+		sample_count = 2;
+		samples = new double[sample_count * 2]{ 1.0, 2.0 };
+		outputs = new double[sample_count] { 2, 3 };
+		break;
+
+	case NonLinearSimple2D:
+		sample_count = 3;
+		samples = new double[sample_count * 2]{ 1.0, 2.0, 3.0 };
+		outputs = new double[sample_count] { 2, 3, 2.5 };
+		break;
+
+	case LinearSimple3D:
+		sample_count = 3;
+		samples = new double[sample_count * 2]{ 1.0, 1.0, 2.0, 2.0, 3.0, 1.0 };
+		outputs = new double[sample_count] { 2, 3, 2.5 };
+		break;
+
+	case LinearTricky3D:
+		sample_count = 3;
+		samples = new double[sample_count * 2]{ 1.0, 1.0, 2.0, 2.0, 3.0, 3.0 };
+		outputs = new double[sample_count] { 1, 2, 3 };
+		break;
+
+	case NonLinearSimple3D:
+		sample_count = 4;
+		samples = new double[sample_count * 2]{ 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0 };
+		outputs = new double[sample_count] { 2, 1, -2, -1 };
+		break;
+
+	default:
+		samples = new double[0];
+		outputs = new double[0];
+		break;
+	}
+}
+
+void CasTest::DisplayInfos()
+{
+	std::cout.precision(5);
+
+	std::cout << "Samples count : " << sample_count <<std::endl;
+	std::cout << "   Samples     |      Output " << std::endl;
+	for (size_t i = 0; i < sample_count; i++)
+	{
+		std::cout << samples[i * 2] << ", " << samples[i * 2 + 1] << " | " << outputs[i] << std::endl;
+	}
+}
---
+++ Unity/Assets/Scripts/TestClass.cs
@@ -0,0 +1,254 @@
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public enum TypeTest
+{
+    LinearSimple,
+    LinearMultiple,
+    XOR,
+    Cross,
+    MultiLinear,
+    MultiCross,
+    LinearSimple2D,
+    NonLinearSimple2D,
+    LinearSimple3D,
+    LinearTricky3D,
+    NonLinearSimple3D
+}
+
+public struct TestInfos
+{
+    public int LayerCount;
+    public int OutputSize;
+    public int[] Dimensions;
+}
+
+
+public class TestClass
+{
+    public int SampleCount;
+
+    public double[] Samples;
+    public double[] Outputs;
+
+    private TypeTest _type;
+
+    public TestInfos Infos;
+    
+    public TestClass(TypeTest type)
+    {
+        _type = type;
+        Infos = new TestInfos();
+        
+        switch (type)
+        {
+            case TypeTest.LinearSimple:
+                SampleCount = 3;
+                Samples = new double[] {1.0f, 1.0f, 2.0f, 3.0f, 3.0f, 3.0f};
+                Outputs = new double[] {1, -1, -1};
+                
+                Infos = new TestInfos()
+                {
+                    LayerCount = 2,
+                    OutputSize = 3,
+                    Dimensions = new int[] {2, 1}
+                };
+                break;
+            
+            case TypeTest.LinearMultiple:
+                SampleCount = 100;
+                Samples = new double[SampleCount * 2];
+                for (int i = 0; i < SampleCount * 2; ++i)
+                {
+                    if (i < 100)
+                        Samples[i] = Random.Range(0.0f, 1.0f) / double.MaxValue + 1.0f;
+                    else 
+                        Samples[i] = Random.Range(0.0f, 1.0f) / double.MaxValue + 2.0f;
+                }
+
+                Outputs = new double[SampleCount];
+                for (int i = 0; i < SampleCount; ++i)
+                {
+                    if (i < 50)
+                        Outputs[i] = 1.0f;
+                    else
+                        Outputs[i] = -1.0f;
+                }
+
+                Infos = new TestInfos() 
+                {
+                    LayerCount = 2,
+                    OutputSize = 100,
+                    Dimensions = new int[] {2, 1}
+                };
+                break;
+            
+            case TypeTest.XOR:
+                SampleCount = 4;
+                Samples = new double[] {1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f};
+                Outputs = new double[] {1, 1, -1, -1};
+                
+                Infos = new TestInfos()
+                {
+                    LayerCount = 3,
+                    OutputSize = 4,
+                    Dimensions = new int[] {2, 3, 1}
+                };
+                break;
+            
+            case TypeTest.Cross:
+                SampleCount = 500;
+                Samples = new double[SampleCount * 2];
+                for (int i = 0; i < SampleCount * 2; ++i)
+                {
+                    Samples[i] = Random.Range(0.0f, 1.0f) / double.MaxValue * 2.0f - 1.0f;
+                }
+                
+                Outputs = new double[SampleCount];
+                for (int i = 0; i < SampleCount; ++i)
+                {
+                    if (Mathf.Abs((float)Samples[i * 2]) <= 0.3f || Mathf.Abs((float)Samples[i * 2 + 1]) <= 0.3f )
+                        Outputs[i] = 1.0f;
+                    else
+                        Outputs[i] = -1.0f;
+                }
+                
+                Infos = new TestInfos()
+                {
+                    LayerCount = 3,
+                    OutputSize = 1000,
+                    Dimensions = new int[] {2, 4, 1}
+                };
+                break;
+            
+            case TypeTest.MultiLinear:
+                //TODO : update needed
+                SampleCount = 3;
+                Samples = new double[] {1.0f, 1.0f, 2.0f, 3.0f, 3.0f, 3.0f};
+                Outputs = new double[] {1, -1, -1};
+                
+                Infos = new TestInfos()
+                {
+                    //TODO : update needed
+                    LayerCount = 2,
+                    OutputSize = 3,
+                    Dimensions = new int[] {2, 1}
+                };
+                break;
+            
+            case TypeTest.MultiCross:
+                //TODO : update needed
+                SampleCount = 3;
+                Samples = new double[] {1.0f, 1.0f, 2.0f, 3.0f, 3.0f, 3.0f};
+                Outputs = new double[] {1, -1, -1};
+                
+                Infos = new TestInfos()
+                {
+                    //TODO : update needed
+                    LayerCount = 2,
+                    OutputSize = 3,
+                    Dimensions = new int[] {2, 1}
+                };
+                break;
+            
+            case TypeTest.LinearSimple2D:
+                //TODO : update needed
+                SampleCount = 2;
+                Samples = new double[] {1.0f, 1.0f, 2.0f, 3.0f, 3.0f, 3.0f};
+                Outputs = new double[] {1, -1, -1};
+                Infos = new TestInfos()
+                {
+                    LayerCount = 2,
+                    OutputSize = 2,
+                    Dimensions = new int[] {1, 1}
+                };
+                break;
+            
+            case TypeTest.NonLinearSimple2D:
+                //TODO : update needed
+                SampleCount = 3;
+                Samples = new double[] {1.0f, 1.0f, 2.0f, 3.0f, 3.0f, 3.0f};
+                Outputs = new double[] {1, -1, -1};
+                
+                Infos = new TestInfos()
+                {
+                    //TODO : update needed
+                    LayerCount = 2,
+                    OutputSize = 3,
+                    Dimensions = new int[] {2, 1}
+                };
+                break;
+            
+            case TypeTest.LinearSimple3D:
+                //TODO : update needed
+                SampleCount = 3;
+                Samples = new double[] {1.0f, 1.0f, 2.0f, 3.0f, 3.0f, 3.0f};
+                Outputs = new double[] {1, -1, -1};
+                
+                Infos = new TestInfos()
+                {
+                    LayerCount = 2,
+                    OutputSize = 3,
+                    Dimensions = new int[] {2, 1}
+                };
+                break;
+            
+            case TypeTest.LinearTricky3D:
+                //TODO : update needed
+                SampleCount = 3;
+                Samples = new double[] {1.0f, 1.0f, 2.0f, 3.0f, 3.0f, 3.0f};
+                Outputs = new double[] {1, -1, -1};
+                
+                Infos = new TestInfos()
+                {
+                    LayerCount = 2,
+                    OutputSize = 3,
+                    Dimensions = new int[] {2, 1}
+                };
+                break;
+            
+            case TypeTest.NonLinearSimple3D:
+                //TODO : update needed
+                SampleCount = 3;
+                Samples = new double[] {1.0f, 1.0f, 2.0f, 3.0f, 3.0f, 3.0f};
+                Outputs = new double[] {1, -1, -1};
+                
+                Infos = new TestInfos()
+                {
+                    LayerCount = 3,
+                    OutputSize = 4,
+                    Dimensions = new int[] {2, 2, 1}
+                };
+                break;
+            
+            default:
+                Debug.Log("Create a LinearSimple as default value for test");
+                SampleCount = 3;
+                Samples = new double[] {1.0f, 1.0f, 2.0f, 3.0f, 3.0f, 3.0f};
+                Outputs = new double[] {1, -1, -1};
+                
+                Infos = new TestInfos()
+                {
+                    LayerCount = 2,
+                    OutputSize = 3,
+                    Dimensions = new int[] {2, 1}
+                };
+                break;
+        }
+    }
+    
+    public void DisplayInfos()
+    {
+        Debug.LogFormat("Type of the test : {0}", _type.ToString());
+        Debug.Log("SAMPLES :");
+        for (int i = 0; i < SampleCount; ++i)
+        {
+            Debug.LogFormat("Sample {0} : ({1}, {2})", i, Samples[i * 2], Samples[i * 2 + 1]);
+        }
+        Debug.Log("OUTPUTS :");
+        for (int i = 0; i < Outputs.Length; ++i)
+        {
+            Debug.LogFormat("Output {0} : {1}", i, Outputs[i]);
+        }
+    }
+}

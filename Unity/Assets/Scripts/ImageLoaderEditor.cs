e6e8b7a76a555d676f0dda325e3a5d3e7c7ec7aa
Attika <attika.mayou@gmail.com>
2021-01-17 19:31:09 +0100

    Load Images from dataset

---
+++ Unity/Assets/Scripts/ImageLoaderEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ImageLoader))]
+public class ImageLoaderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ImageLoader loader = (ImageLoader) target;
+        if (GUILayout.Button("Import Images"))
+        {
+            loader.InitializeImages();
+        }
+    }
+}
